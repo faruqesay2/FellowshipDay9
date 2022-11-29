7aab8dbd9ff88657c25aa7d3ff7766145b0fbe35
FARUQESAY <113195467+faruqesay2@users.noreply.github.com>
2022-11-29 19:38:22 +0530

    UC_5

---
+++ Uc1AddressBook/Uc1AddressBook/AddMultipleContact.cs
@@ -0,0 +1,74 @@
+namespace AddressBookManagementSystem
+{
+    class Program   
+    {
+        public static List<Contact> contactList = new List<Contact>();
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Sytem.");
+
+            CreatingContacts();
+            DisplayContacts();
+        }
+        public static void FillingDetails(Contact contact)
+        {
+            Console.WriteLine("Enter first name: ");
+            contact.firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter last name: ");
+            contact.lastName = Console.ReadLine();
+
+            Console.WriteLine("Enter address: ");
+            contact.address = Console.ReadLine();
+
+            Console.WriteLine("Enter city: ");
+            contact.city = Console.ReadLine();
+
+            Console.WriteLine("Enter state: ");
+            contact.state = Console.ReadLine();
+
+            Console.WriteLine("Enter phone: ");
+            contact.phone = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter email: ");
+            contact.email = Console.ReadLine();
+
+            Console.WriteLine("Enter zipcode: ");
+            contact.zipcode = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public static void CreatingContacts()
+        {
+            Console.WriteLine("Do you want to add new contact press 1 or press 2 to cancle.");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+
+            while (num == 1)
+            {
+                Contact contact = new Contact();
+                FillingDetails(contact);
+                contactList.Add(contact);
+
+                Console.WriteLine("Do you want to add anoter contact then press 1 or press 2 for exit ");
+                num = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("=============================================================");
+            Console.WriteLine("Total number of contact in address book:" + contactList.Count);
+        }
+
+        public static void DisplayContacts()
+        {
+            //print contacts
+
+            Console.WriteLine("=============================================================");
+            Console.WriteLine("Current contacts in adress book:");
+
+            foreach (Contact contact in contactList)
+            {
+                Console.WriteLine(contact.firstName);
+            }
+            Console.WriteLine("=============================================================");
+
+        }
+    }
+}
